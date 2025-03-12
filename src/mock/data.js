9a766fbb1ed1f30efd36035854296888d1c91df8
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-03-12 23:54:26 +0300

    add layout and styles

---
+++ src/mock/data.js
@@ -0,0 +1,93 @@
+export const cardList = [
+  {
+    id: 1,
+    theme: "Web Design",
+    title: "Название задачи",
+    date: "30.10.23",
+    status: "noStatus",
+  },
+  {
+    id: 2,
+    theme: "Research",
+    title: "Название задачи",
+    date: "30.10.23",
+    status: "noStatus",
+  },
+  {
+    id: 3,
+    theme: "Web Design",
+    title: "Название задачи",
+    date: "30.10.23",
+    status: "noStatus",
+  },
+  {
+    id: 4,
+    theme: "Copywriting",
+    title: "Название задачи",
+    date: "30.10.23",
+    status: "noStatus",
+  },
+  {
+    id: 5,
+    theme: "Research",
+    title: "Название задачи",
+    date: "30.10.23",
+    status: "noStatus",
+  },
+  {
+    id: 6,
+    theme: "Research",
+    title: "Название задачи",
+    date: "30.10.23",
+    status: "needToDo",
+  },
+  {
+    id: 7,
+    theme: "Research",
+    title: "Название задачи",
+    date: "30.10.23",
+    status: "inProcess",
+  },
+  {
+    id: 8,
+    theme: "Copywriting",
+    title: "Название задачи",
+    date: "30.10.23",
+    status: "inProcess",
+  },
+  {
+    id: 9,
+    theme: "Web Design",
+    title: "Название задачи",
+    date: "30.10.23",
+    status: "inProcess",
+  },
+  {
+    id: 10,
+    theme: "Research",
+    title: "Название задачи",
+    date: "30.10.23",
+    status: "test",
+  },
+  {
+    id: 11,
+    theme: "Research",
+    title: "Название задачи",
+    date: "30.10.23",
+    status: "ready",
+  },
+];
+
+export const theme = {
+  WebDesign: "#ffe4c2",
+  Research: "#b4fdd1",
+  Copywriting: "#e9d4ff",
+};
+
+export const cardStatuses = {
+  noStatus: "Без статуса",
+  needToDo: "Нужно сделать",
+  inProcess: "В работе",
+  test: "Тестирование",
+  ready: "Готово",
+};
