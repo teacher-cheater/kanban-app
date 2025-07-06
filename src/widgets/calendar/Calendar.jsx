67f36ec9aba9d5fdc348d61687d6d83c0b33d88e
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-07-06 23:16:09 +0300

    calendar form edits

--- src/widgets/calendar/Calendar.jsx
+++ src/widgets/calendar/Calendar.jsx
@@ -1,20 +1,29 @@
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
+import cls from "./Calendar.module.scss";
 
 export default function Calendar(props) {
-  const { selected, setSelected, task } = props;
-  console.log("selected", selected);
+  const { selected, setSelected } = props;
 
   return (
     <DayPicker
+      className={cls.calendar}
       animate
       mode="single"
       selected={selected}
       onSelect={setSelected}
+      weekStartsOn={1}
+      locale="ru"
+      formatters={{
+        formatCaption: (date) => {
+          return new Intl.DateTimeFormat("ru-RU", {
+            month: "long",
+            year: "numeric",
+          }).format(date);
+        },
+      }}
       footer={
-        selected
-          ? `Selected: ${selected.toLocaleDateString()}`
-          : "Выберите дату"
+        selected ? `Выбрано: ${selected.toLocaleDateString()}` : "Выберите дату"
       }
     />
   );
