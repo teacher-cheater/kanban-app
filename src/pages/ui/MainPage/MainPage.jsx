a0e985dbc4f004f83f3df7be697c77569ef12bcf
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-07-08 01:26:38 +0300

    fix update task

--- src/pages/ui/MainPage/MainPage.jsx
+++ src/pages/ui/MainPage/MainPage.jsx
@@ -1,9 +1,10 @@
-import PopBrowse from "../../../shared/popup/PopBrowse";
+import { Outlet } from "react-router-dom";
 import Header from "../../../widgets/header/ui/Header/Header";
 import Main from "../../../widgets/main/Main/Main";
 export default function MainPage() {
   return (
     <div className="wrapper">
+      <Outlet />
       <Header />
       <Main />
     </div>
